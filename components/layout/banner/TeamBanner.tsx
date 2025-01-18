e352a9e760464dda8eab7c56dc82b7c8dad4b4e1
Rajeevaletshanth <vjvfc2k16@gmail.com>
2025-01-19 00:10:40 +0530

    dark rogers update

--- components/layout/banner/TeamBanner.tsx
+++ components/layout/banner/TeamBanner.tsx
@@ -5,6 +5,9 @@ import titanLogo from "@/public/images/teams/logo/Titans.png";
 import codeRedBanner from "@/public/images/teams/banners/Code Red Banner.jpg";
 import codeRedLogo from "@/public/images/teams/logo/Code Red.png";
 
+import darkRogersBanner from "@/public/images/teams/banners/Dark Rogers Banner.jpg";
+import darkRogersLogo from "@/public/images/teams/logo/Dark Rogers.png";
+
 import Image from 'next/image';
 import AboutCraft from '@/components/containers/about/AboutCraft';
 import TeamCraft from '@/components/containers/about/TeamCraft';
@@ -39,6 +42,19 @@ const TeamBanner = () => {
           
       </div>
       <TeamCraft no={2} name={'Code Red'} players="Rajeev / Carson" rank={'N/A'} logo={codeRedLogo} color='red'/>
+      </>
+      <>
+      <AboutTextSlider text="DARK ROGERS"/>
+      <div className=" parallax-img text-center">
+        <Image
+          src={darkRogersBanner}
+          alt="Team Banner"
+          priority
+          layout="responsive"
+        />
+          
+      </div>
+      <TeamCraft no={3} name={'Dark Rogers'} players="Maxwel / Premthas" rank={'N/A'} logo={darkRogersLogo} color='#712ebe'/>
       </>
     </section>
   );
