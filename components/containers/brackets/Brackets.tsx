4842f5e6b3004684f81dade034e25eed400f6002
Rajeevaletshanth <vjvfc2k16@gmail.com>
2025-01-30 14:29:04 +0530

    rtf upd

--- components/containers/brackets/Brackets.tsx
+++ components/containers/brackets/Brackets.tsx
@@ -8,7 +8,6 @@ import Shape from "@/public/images/footer/shape-one.png";
 import { useEffect, useState, useRef } from "react";
 import VersusCard from "./components/VersusCard";
 import "./components/VersusCard.css";
-import { jsPlumb } from "jsplumb";
 import bgimg from '@/public/images/RTFLines.png'
 
 const teamArray = [
@@ -46,68 +45,68 @@ const Brackets = () => {
   const finalRef = useRef<any>(null);
   const final2Ref = useRef<any>(null);
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setWidth(window.innerWidth);
-    }
+  // useEffect(() => {
+  //   if (typeof window !== 'undefined') {
+  //     setWidth(window.innerWidth);
+  //   }
 
-    const instance: any = jsPlumb.getInstance(); // Corrected API usage
+  //   const instance: any = jsPlumb.getInstance(); // Corrected API usage
 
-    // Add connections between "Qualifier 1" and "Final"
-    instance.connect({
-      source: qualifier1Ref.current,
-      target: finalRef.current,
-      anchors: ["Center", "Center"],
-      connector: "Straight",
-      paintStyle: { stroke: "white", strokeWidth: 3 },
-      endpoint: "Blank"
-      // overlays: [
-      //   ["Arrow", { location: 1, width: 10, length: 10 }]
-      // ]
-    });
+  //   // Add connections between "Qualifier 1" and "Final"
+  //   instance.connect({
+  //     source: qualifier1Ref.current,
+  //     target: finalRef.current,
+  //     anchors: ["Center", "Center"],
+  //     connector: "Straight",
+  //     paintStyle: { stroke: "white", strokeWidth: 3 },
+  //     endpoint: "Blank"
+  //     // overlays: [
+  //     //   ["Arrow", { location: 1, width: 10, length: 10 }]
+  //     // ]
+  //   });
 
-    instance.connect({
-      source: qualifier1Ref.current,
-      target: qualifier2Top1Ref.current,
-      anchors: ["Center", "Center"],
-      connector: "Straight",
-      paintStyle: { stroke: "white", strokeWidth: 3 },
-      endpoint: "Blank",
-      // overlays: [
-      //   ["Arrow", { location: 1, width: 10, length: 10 }]
-      // ]
-    });
+  //   instance.connect({
+  //     source: qualifier1Ref.current,
+  //     target: qualifier2Top1Ref.current,
+  //     anchors: ["Center", "Center"],
+  //     connector: "Straight",
+  //     paintStyle: { stroke: "white", strokeWidth: 3 },
+  //     endpoint: "Blank",
+  //     // overlays: [
+  //     //   ["Arrow", { location: 1, width: 10, length: 10 }]
+  //     // ]
+  //   });
 
-    instance.connect({
-      source: qualifier2Top1Ref.current,
-      target: qualifier2Top2Ref.current,
-      anchors: ["Center", "Center"],
-      connector: "Straight",
-      paintStyle: { stroke: "white", strokeWidth: 3 },
-      endpoint: "Blank",
-      overlays: [
-        ["Arrow", { location: 1, width: 10, length: 10 }]
-      ]
-    });
+  //   instance.connect({
+  //     source: qualifier2Top1Ref.current,
+  //     target: qualifier2Top2Ref.current,
+  //     anchors: ["Center", "Center"],
+  //     connector: "Straight",
+  //     paintStyle: { stroke: "white", strokeWidth: 3 },
+  //     endpoint: "Blank",
+  //     overlays: [
+  //       ["Arrow", { location: 1, width: 10, length: 10 }]
+  //     ]
+  //   });
 
-    instance.connect({
-      source: finalRef.current,
-      target: final2Ref.current,
-      anchors: ["Center", "Center"],
-      connector: "Straight",
-      paintStyle: { stroke: "white", strokeWidth: 3 },
-      endpoint: "Blank",
-      overlays: [
-        ["Arrow", { location: 1, width: 10, length: 10 }]
-      ]
-    });
+  //   instance.connect({
+  //     source: finalRef.current,
+  //     target: final2Ref.current,
+  //     anchors: ["Center", "Center"],
+  //     connector: "Straight",
+  //     paintStyle: { stroke: "white", strokeWidth: 3 },
+  //     endpoint: "Blank",
+  //     overlays: [
+  //       ["Arrow", { location: 1, width: 10, length: 10 }]
+  //     ]
+  //   });
 
-    // Cleanup on unmount
-    return () => {
-      instance.deleteEveryConnection();
-      instance.reset();
-    };
-  }, []);
+  //   // Cleanup on unmount
+  //   return () => {
+  //     instance.deleteEveryConnection();
+  //     instance.reset();
+  //   };
+  // }, []);
 
   return (
     <section className="mt-4 pb-0 craft">
