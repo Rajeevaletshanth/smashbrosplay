32d4d3a120d2d06907160e660fc6a4d4dd219985
Rajeevaletshanth <vjvfc2k16@gmail.com>
2025-04-17 13:06:04 +0530

    Registered teams

--- components/layout/banner/TeamBanner.tsx
+++ components/layout/banner/TeamBanner.tsx
@@ -1,21 +1,7 @@
 import React from 'react';
-import titanBanner from "@/public/images/teams/banners/Titans Banner.jpg";
-import titanLogo from "@/public/images/teams/logo/Titans.png";
+import sharksBanner from "@/public/images/teams/banners/Sharks Cover.jpg";
+import sharksLogo from "@/public/images/teams/logo/Sharks.png";
 
-import codeRedBanner from "@/public/images/teams/banners/Code Red Banner.jpg";
-import codeRedLogo from "@/public/images/teams/logo/Code Red.png";
-
-import darkRogersBanner from "@/public/images/teams/banners/Dark Rogers Banner.jpg";
-import darkRogersLogo from "@/public/images/teams/logo/Dark Rogers.png";
-
-import teamBaddiesBanner from "@/public/images/teams/banners/Team Baddies Banner.jpg";
-import teamBaddiesLogo from "@/public/images/teams/logo/Team Baddies.png";
-
-import smashersBanner from "@/public/images/teams/banners/Smashers Banner.jpg";
-import smashersLogo from "@/public/images/teams/logo/Smashers.png";
-
-import venpuraBanner from "@/public/images/teams/banners/Venpura Banner.jpg";
-import venpuraLogo from "@/public/images/teams/logo/Venpura.png";
 
 import Image from 'next/image';
 import AboutCraft from '@/components/containers/about/AboutCraft';
@@ -26,84 +12,20 @@ const TeamBanner = () => {
   return (
     <section className='section bg-black'>
       <>
-      <AboutTextSlider text="TITANS"/>
+      <AboutTextSlider text="SHARKS"/>
       <div className=" parallax-img text-center">
         <Image
-          src={titanBanner}
+          src={sharksBanner}
           alt="Team Banner"
           priority
           layout="responsive"
         />
           
       </div>
-      <TeamCraft no={1} name={'Titans'} players="Justin / Ruban" rank={'N/A'} logo={titanLogo} color='yellow'/>
+      {/* <TeamCraft no={1} name={'SHARKS'} players="Thenushkan / Millington" rank={'N/A'} logo={sharksLogo} color='#0a63bd'/> */}
       
       </>
-      <>
-      <AboutTextSlider text="CODE RED"/>
-      <div className=" parallax-img text-center">
-        <Image
-          src={codeRedBanner}
-          alt="Team Banner"
-          priority
-          layout="responsive"
-        />
-          
-      </div>
-      <TeamCraft no={2} name={'Code Red'} players="Rajeev / Carson" rank={'N/A'} logo={codeRedLogo} color='red'/>
-      </>
-      <>
-      <AboutTextSlider text="DARK ROGERS"/>
-      <div className=" parallax-img text-center">
-        <Image
-          src={darkRogersBanner}
-          alt="Team Banner"
-          priority
-          layout="responsive"
-        />
-          
-      </div>
-      <TeamCraft no={3} name={'Dark Rogers'} players="Maxwel / Premthas" rank={'N/A'} logo={darkRogersLogo} color='#712ebe'/>
-      </>
-      <>
-      <AboutTextSlider text="TEAM BADDIES"/>
-      <div className=" parallax-img text-center">
-        <Image
-          src={teamBaddiesBanner}
-          alt="Team Banner"
-          priority
-          layout="responsive"
-        />
-          
-      </div>
-      <TeamCraft no={4} name={'Team Baddies'} players="Bona / Thusyanthan" rank={'N/A'} logo={teamBaddiesLogo} color='#d62750'/>
-      </>
-      <>
-      <AboutTextSlider text="SMASHERS"/>
-      <div className=" parallax-img text-center">
-        <Image
-          src={smashersBanner}
-          alt="Team Banner"
-          priority
-          layout="responsive"
-        />
-          
-      </div>
-      <TeamCraft no={5} name={'Smashers'} players="Diron / Dilushan" rank={'N/A'} logo={smashersLogo} color='#7103a4'/>
-      </>
-      <>
-      <AboutTextSlider text="VENPURA"/>
-      <div className=" parallax-img text-center">
-        <Image
-          src={venpuraBanner}
-          alt="Team Banner"
-          priority
-          layout="responsive"
-        />
-          
-      </div>
-      <TeamCraft no={6} name={'Venpura'} players="Kathires / Kumar" rank={'N/A'} logo={venpuraLogo} color='#ffffff'/>
-      </>
+
     </section>
   );
 };
