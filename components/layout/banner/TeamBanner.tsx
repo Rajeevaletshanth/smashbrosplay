78fba5a351b926ed626a37f79935931af76ec123
Rajeevaletshanth <vjvfc2k16@gmail.com>
2025-04-18 22:57:42 +0530

    2 teams added

--- components/layout/banner/TeamBanner.tsx
+++ components/layout/banner/TeamBanner.tsx
@@ -5,6 +5,12 @@ import sharksLogo from "@/public/images/teams/logo/Sharks.png";
 import birdiesBanner from "@/public/images/teams/banners/Birdies Cover.jpg";
 import birdiesLogo from "@/public/images/teams/logo/Birdies.png";
 
+import lsBanner from "@/public/images/teams/banners/Lambert's Smash Cover.jpg";
+import lsLogo from "@/public/images/teams/logo/Lambert's Smash.png";
+
+import exboltsBanner from "@/public/images/teams/banners/Exbolts Cover.jpg";
+import exboltsLogo from "@/public/images/teams/logo/Exbolts.png";
+
 import Image from 'next/image';
 import AboutCraft from '@/components/containers/about/AboutCraft';
 import TeamCraft from '@/components/containers/about/TeamCraft';
@@ -32,6 +38,36 @@ const TeamBanner = () => {
       <div className=" parallax-img text-center">
         <Image
           src={birdiesBanner}
+          alt="Team Banner"
+          priority
+          layout="responsive"
+        />
+          
+      </div>
+      {/* <TeamCraft no={2} name={'SHARKS'} players="Rubanraj / Kathires" rank={'N/A'} logo={birdiesLogo} color='#0a63bd'/> */}
+      
+      </>
+
+      <>
+      <AboutTextSlider text="LAMBERT'S SMASH"/>
+      <div className=" parallax-img text-center">
+        <Image
+          src={lsBanner}
+          alt="Team Banner"
+          priority
+          layout="responsive"
+        />
+          
+      </div>
+      {/* <TeamCraft no={2} name={'SHARKS'} players="Rubanraj / Kathires" rank={'N/A'} logo={birdiesLogo} color='#0a63bd'/> */}
+      
+      </>
+
+      <>
+      <AboutTextSlider text="EXBOLTS"/>
+      <div className=" parallax-img text-center">
+        <Image
+          src={exboltsBanner}
           alt="Team Banner"
           priority
           layout="responsive"
