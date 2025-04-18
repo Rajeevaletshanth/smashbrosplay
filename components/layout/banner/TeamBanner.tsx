d14968df84e38a343adf2b86300b5c1f277eafc6
Rajeevaletshanth <vjvfc2k16@gmail.com>
2025-04-18 13:01:08 +0530

    team 2 updated

--- components/layout/banner/TeamBanner.tsx
+++ components/layout/banner/TeamBanner.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import sharksBanner from "@/public/images/teams/banners/Sharks Cover.jpg";
 import sharksLogo from "@/public/images/teams/logo/Sharks.png";
 
+import birdiesBanner from "@/public/images/teams/banners/Birdies Cover.jpg";
+import birdiesLogo from "@/public/images/teams/logo/Birdies.png";
 
 import Image from 'next/image';
 import AboutCraft from '@/components/containers/about/AboutCraft';
@@ -25,7 +27,20 @@ const TeamBanner = () => {
       {/* <TeamCraft no={1} name={'SHARKS'} players="Thenushkan / Millington" rank={'N/A'} logo={sharksLogo} color='#0a63bd'/> */}
       
       </>
-
+      <>
+      <AboutTextSlider text="BIRDIES"/>
+      <div className=" parallax-img text-center">
+        <Image
+          src={birdiesBanner}
+          alt="Team Banner"
+          priority
+          layout="responsive"
+        />
+          
+      </div>
+      {/* <TeamCraft no={2} name={'SHARKS'} players="Rubanraj / Kathires" rank={'N/A'} logo={birdiesLogo} color='#0a63bd'/> */}
+      
+      </>
     </section>
   );
 };
